56d52741544562187c920bedc8c56e8bb70e4ddf
h-spear <80024307+h-spear@users.noreply.github.com>
2025-07-07 23:44:18 +0900

    Update Design & Posting 1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,3 +18,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+const customDefaultPage = () => {
+  const searchTrigger = document.querySelector('#search-trigger-button');
+  const searchBox = document.querySelector('#search-box');
+  const container = document.querySelector('.search-container');
+  searchTrigger.addEventListener('click', () => {
+    searchBox.classList.remove('invisible');
+  });
+  searchBox.addEventListener('click', () => {
+    if (!container.contains(event.target)) {
+      searchBox.classList.add('invisible');
+    }
+  });
+};
+
+customDefaultPage();
